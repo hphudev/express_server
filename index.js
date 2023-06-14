23e9b30c3d5d7be69bd0756db3d4934106cb123d
Lê Hoàng Phú <hphudev.phule@gmail.com>
2023-06-14 22:05:55 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5500
 const {database, firestore} = require('./firebase_module')
 const { onValue, ref } = require('firebase/database')
 const { addDoc, collection } = require('firebase/firestore')
